818e267ff745092e547057ec04523d0511e6fa11
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-11-20 17:48:07 -0500

    Update get colors

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 /*import { getResistorOhms } from './resistor.js'*/
 
-let firstBandContainer = document.querySelector('[data-first-band]');
+let bandContainer = document.querySelector('[data-container]');
+
 let displayFirstBandContainer = document.querySelector('[data-display-first-band]');
-
-let secondBandContainer = document.querySelector('[data-second-band]');
 let displaySecondBandContainer = document.querySelector('[data-display-second-band]');
-
-let multiplierBandContainer = document.querySelector('[data-multiplier-band]');
 let displayMultiplierBandContainer = document.querySelector('[data-display-multiplier-band]');
-
-let resistanceBandContainer = document.querySelector('[data-resistance-band]');
 let displayResistanceBandContainer = document.querySelector('[data-display-resistance-band]');
 
-firstBandContainer.addEventListener('click', e => {
-    displayFirstBandContainer.classList.replace(displayFirstBandContainer.classList[1], e.target.classList[1]);
-})
-
-secondBandContainer.addEventListener('click', e => {
-    displaySecondBandContainer.classList.replace(displaySecondBandContainer.classList[1], e.target.classList[1]);
-})
-
-multiplierBandContainer.addEventListener('click', e => {
-    displayMultiplierBandContainer.classList.replace(displayMultiplierBandContainer.classList[1], e.target.classList[1]);
-})
-
-resistanceBandContainer.addEventListener('click', e => {
-    displayResistanceBandContainer.classList.replace(displayResistanceBandContainer.classList[1], e.target.classList[1]);
+bandContainer.addEventListener('click', e => {
+    console.log(e.target.classList[1]);
+    switch (e.target.parentNode.getAttribute('data-band')) {
+        case "1":
+            displayFirstBandContainer.classList.replace(displayFirstBandContainer.classList[1], e.target.classList[1]);
+            break;
+        case "2":
+            displaySecondBandContainer.classList.replace(displaySecondBandContainer.classList[1], e.target.classList[1]);
+            break;
+        case "3":
+            displayMultiplierBandContainer.classList.replace(displayMultiplierBandContainer.classList[1], e.target.classList[1]);
+            break;
+        case "4":
+            displayResistanceBandContainer.classList.replace(displayResistanceBandContainer.classList[1], e.target.classList[1]);
+            break;
+    }
 })
