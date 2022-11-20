ad1bd98a415ca777c1c47e0c7d7710642132f4f0
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-11-20 17:20:58 -0500

    Color changing first band

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-import { getResistorOhms } from './resistor.js'
+/*import { getResistorOhms } from './resistor.js'*/
+
+let firstBandContainer = document.querySelector('[data-first-band]');
+let displayFirstBandContainer = document.querySelector('[data-display-first-band]');
+
+let firstBandColor = 'Black';
+let secondBandColor = 'Black';
+let multiplierColor = 'Black';
+let resistanceColor = 'Black';
+
+firstBandContainer.addEventListener('click', e => {
+    displayFirstBandContainer.classList.replace(displayFirstBandContainer.classList[1], e.target.classList[1]);
+})
